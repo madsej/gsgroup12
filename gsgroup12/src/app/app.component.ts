3cfd0bda2d4bde23a1cb2f1b5e9eac32d4063ffe
madsej <nat4ek@virginia.edu>
2025-01-20 13:19:13 +0100

    has a search bar that says "filter by city" to rush make sure you enter "cd gsgroup12" before typing "ng serve"

--- gsgroup12/src/app/app.component.ts
+++ gsgroup12/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  //standalone: true,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['../gsgroup12/app.component.css'],
 })
 export class AppComponent {
-  title = 'gsgroup12';
+  title = 'homes';
 }
+
