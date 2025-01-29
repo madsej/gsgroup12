7eb54e4a0aa42888d7d08838c6cc0d16e9ba9ebe
Felix Ofori <99497710+Cletches@users.noreply.github.com>
2025-01-29 06:01:12 -0600

    app.module.ts

--- gsgroup12/src/app.module.ts
+++ gsgroup12/src/app.module.ts
@@ -1,15 +1,38 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { HomeComponent } from './home/home.component';
+
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     AppComponent,   // Import AppComponent here (instead of declarations)
+//     HomeComponent,  // Import HomeComponent here (instead of declarations)
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent],  // Bootstrap the AppComponent
+// })
+// export class AppModule {}
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule here
+import { AppComponent } from './app.component';
+import { FundSelectorComponent } from './fund-selector.component';
+import { CalculatorComponent } from './calculator.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    FundSelectorComponent,
+    CalculatorComponent
+  ],
   imports: [
     BrowserModule,
-    AppComponent,   // Import AppComponent here (instead of declarations)
-    HomeComponent,  // Import HomeComponent here (instead of declarations)
+    FormsModule,         // Add FormsModule here
+    HttpClientModule     // If not already imported
   ],
   providers: [],
-  bootstrap: [AppComponent],  // Bootstrap the AppComponent
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
