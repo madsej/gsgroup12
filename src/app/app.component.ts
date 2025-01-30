1ea5fdde754b8bad230cf0d6986812740b681244
Praise Manzi <praisemanzi@gmail.com>
2025-01-30 00:56:08 -0600

    fixed backend issues

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `<h1>Welcome to My Angular App</h1>`,
+    standalone: false,
+    styles: [`h1 { font-family: Lato; }`]
+})
+export class AppComponent { }
